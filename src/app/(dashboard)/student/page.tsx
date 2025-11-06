3ba4ae2547116e4c2dd7c009aac1a188a1ec5895
hao0834236849 <hao362003@gmail.com>
2025-11-06 23:00:43 +0700

    Project School Management

---
+++ src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import Announcements from "@/components/Announcements";
+import EventCalendar from "@/components/EventCalendar";
+
+// ✅ Import BigCalendar với SSR tắt
+const BigCalendar = dynamic(() => import("@/components/BigCalender"), {
+  ssr: false,
+});
+
+const StudentPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default StudentPage;
